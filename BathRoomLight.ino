9a73c15fadd352125017a8db0eb4f7f48989920c
AMurchick <a@i-vedro.ru>
2014-08-23 22:42:28 +0700

    лобавлена IR библиотека, таймер для отсчета времени и плавный вкл/выкл светодиодов

--- BathRoomLight.ino
+++ BathRoomLight.ino
@@ -1,13 +1,116 @@
+﻿/*
+	WARNING - нужно добавить в IRremoteTools.h строчку:
+		#define TKD2 2
+	где 2 - номер пина, куда воткнут IR приемник
+*/
+
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <IRremoteTools.h>
+
+#include "remoteCodes.h"
+
+#define	LED	5
+#define	IR	2
+
 void setup()
 {
+	digitalWrite(LED, 0);
+	pinMode(LED, OUTPUT);
 
-  /* add setup code here */
+	Serial.begin(115200);
+	beginIRremote();
+	digitalWrite(IR, HIGH);
+	irparams.blinkflag = true;
+	cli();
 
+	// Библиотека IRremote на ардуино нано 328 юзает таймер 2, канал OC2A - мы будем юзать канал OC2B
+	// Библиотека IRremote настроило прерывание вызываться 1 раз в 0.5 микросекунд - каунтер установим в 100 - будем вызываться раз в 50 мксек
+	OCR2B = 100;
+	// Разрешить прерывания таймера 2 по каналу OC2B
+	TIMSK2 |= _BV(OCIE2B);
+
+	sei();
+	Serial.println(TCCR2B, HEX);
 }
+
+volatile unsigned int timerCounter = 0;
+volatile unsigned int timerCounterOneForSecond = 0;
+volatile unsigned int timerCounterOneForHalfSecond = 0;
+volatile unsigned long uptime = 0;
+volatile long prevIr = 0;
+volatile unsigned int ledOn = false;
+volatile unsigned int ledLevel = 0;
+
+// Вызывается 1 раз в 0.00005 сек
+ISR(TIMER2_COMPB_vect) {
+
+	timerCounter++;
+
+	// Если прошло 0.01 секунды
+	if (timerCounter == 200) {
+		timerCounter = 0;
+		timerCounterOneForSecond++;
+		if (timerCounterOneForHalfSecond)
+			timerCounterOneForHalfSecond++;
+
+		if (ledOn && ledLevel != 255) {
+			ledLevel++;
+			analogWrite(LED, ledLevel);
+		}
+		if (!ledOn && ledLevel != 0) {
+			ledLevel--;
+			analogWrite(LED, ledLevel);
+		}
+	}
+
+	// Если прошло пол секунды
+	if (timerCounterOneForHalfSecond == 50) {
+		timerCounterOneForHalfSecond = 0;
+		prevIr = 0;
+	}
+
+	// Если прошла секунда
+	if (timerCounterOneForSecond == 100) {
+		timerCounterOneForSecond = 0;
+		uptime++;
+	}
+}
+
+unsigned long isIrReceived() {
+
+	unsigned long ir;
+
+	if (IRrecived()) {
+		ir = getIRresult();
+		resumeIRremote();
+		if (ir != prevIr) {
+			prevIr = ir;
+			timerCounterOneForHalfSecond = 1;
+			return ir;
+		}
+	}
+
+	return 0;
+}
+
+unsigned long tmp = 0;
+unsigned long irIn;
 
 void loop()
 {
+	if (irIn = isIrReceived()) {
+		Serial.println(irIn, HEX);
+		if (irIn == LIGTH_HIGH || irIn == LIGTH_LOW)
+			Serial.println(analogRead(A0));
 
-  /* add main program code here */
+		if (irIn == POWER) {
+			ledOn = !ledOn;
+		}
+	}
 
+	if (tmp != uptime) {
+		//Serial.println(uptime);
+		tmp = uptime;
+	}
 }
---
+++ remoteCodes.h
@@ -0,0 +1,21 @@
+﻿//
+// Определение кнопок пульта от SONY TV
+//
+
+
+#define POWER		0xF124B0B6
+
+#define LIGTH_HIGH	0x82D52197	// символ полупустого экрана - запомнить яркость со светом
+#define LIGTH_LOW	0x178FFA01	// символ пустого экрана - запомнить яркость без света
+
+#define DIG0	0x3B0B8330
+#define DIG1	0x207BEF0F
+#define DIG2	0xE8455D8E
+#define DIG3	0xCBB7E949
+#define DIG4	0x2C1F3172
+#define DIG5	0x905ED4F5
+#define DIG6	0x8CE1E3FC
+#define DIG7	0x315B1905
+#define DIG8	0xC67C01B6
+#define DIG9	0xB418D969
+
